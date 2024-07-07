00c6a05009c89cdca95a78f4fdf8168f790f0ef0
mykim8710 <mykim8710@gmail.com>
2024-07-07 22:15:02 +0900

    Springboot java to kotlin 리팩토링 프로젝트 생성

---
+++ src/main/java/com/javatokotlinspring/todo/api/model/TodoRequest.java
@@ -0,0 +1,17 @@
+package com.javatokotlinspring.todo.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoRequest {
+
+    private String title;
+
+    private String description;
+
+    private Boolean done = false;
+}
